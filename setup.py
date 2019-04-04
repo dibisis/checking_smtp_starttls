a703072eb2e532478da111a64070f1e0016a5b44
GoongWon, Chun <dibisis@gmail.com>
2019-04-04 15:54:42 +0900

    Bump version: 0.4.0 → 0.4.0

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dibisis/checking_smtp_starttls',
-    version='0.4.0',
+    version='version='0.4.0'',
     zip_safe=False,
 )
